f7dbdd7ffb9f0bcae30f21609215caee1932aefc
phuc091003 <huuphucnguyen73@gmail.com>
2024-06-02 14:45:29 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/firstmyapp/MainActivity.java
+++ app/src/main/java/com/example/firstmyapp/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.firstmyapp;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +16,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText etDescription, etAddress;
+    Button btnOrder,btnPay;
+    ListView lvOrders;
+    String username;
+    int userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        myDb = new DatabaseHelper(this);
+        etDescription = findViewById(R.id.etDescription);
+        etAddress = findViewById(R.id.btnOrder);
+        btnPay = findViewById(R.id.btnPay);
+        lvOrders = findViewById(R.id.lvOrder);
 
+        username = getIntent().getStringExtra("USERNAME");
+        userId = myDb.getUserIdByUsername(username);
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString().trim();
+                String address = etAddress.getText().toString().trim();
+                if (description.isEmpty() || address.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                boolean isInserted = myDb.insertOrder(description, address, userId);
+                if (isInserted) {
+                    Toast.makeText(MainActivity.this, "Order placed successfully", Toast.LENGTH_SHORT).show();
+                    loadOrder();
+                } else {
+                    Toast.makeText(MainActivity.this, "Order placement failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Payment functionally is not implemented yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+        loadOrder();
+    }
+    private void loadOrder(){
+        Cursor cursor = myDb.getOrdersByUser(userId);
+        String[] form = new String[]{"DESCRIPTION", "ADDRESS"};
+        int[] to = new int[]{R.id.tvOrderDescription, R.id.tvOrderAddress};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.order_item, cursor, form, to, 0);
+        lvOrders.setAdapter(adapter);
     }
 }
